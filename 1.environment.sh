bc1fa627a28b2260a63cbf92733d1a87602f05a8
svzhk1 <39025839+svzhk1@users.noreply.github.com>
2018-06-20 09:01:25 +0530

    environment varible
    first program

---
+++ 1.environment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "lets work with environment variable"
+echo "====================================="
+echo "hello your user name is : $USER"
+echo "==============================="
+echo "your home directorty is : $HOME"
+echo "==============================="
+echo "your shell location is : $SHELL"
+echo "====================================="
+echo "your mail directory location is : $MAIL"
+
+
