4cd36c8741344696e617ea1c057355edf1d1ed4e
svzhk1 <39025839+svzhk1@users.noreply.github.com>
2018-06-20 10:24:59 +0530

    Working with  math

---
+++ 5.math.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "==================="
+
+expr 5 + 2  #make sure you put space correctly
+echo "==================="
+expr 5 - 2
+echo "==================="
+expr 10 / 2
+echo "==================="
+expr 10 \* 2
+echo "==================="
+expr 3 + 5 \* 3
+echo "==================="
+expr \( 3 + 5 \) \* 3
+echo "==================="
+echo "end of script"
+
