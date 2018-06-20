b50769e65ece4afefc615cc53496086fea5ea346
svzhk1 <39025839+svzhk1@users.noreply.github.com>
2018-06-20 09:22:46 +0530

    variable
    variable example

---
+++ 2.variable.sh
@@ -0,0 +1,12 @@
+
+user="Saurav Vaid"
+datec=`date`
+admin="svisback@gmail.com"
+echo " "
+echo "=============================="
+echo "script owner: $user "
+echo "create date : $datec "
+echo "contact admin : $admin "
+echo "=============================="
+echo " "
+
