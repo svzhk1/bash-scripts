5013d9f28b911ab78ab00effc06e9314ad83697b
svzhk1 <39025839+svzhk1@users.noreply.github.com>
2018-06-20 10:36:03 +0530

    special charactor

---
+++ 6.special.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Done Execute program it will not work try these commands on shell bash console directly
+
+echo "start of program "
+echo "================="
+echo """   #i want to echo " that is not possible
+
+echo "\""
+
+echo "================="
+
+echo "\"        # this will not print the \
+echo "\\"
+
+echo "================="
+
+echo "$saurav"   # trhis will not print $saurav
+echo "\$saurav"
+echo '\saurav'
