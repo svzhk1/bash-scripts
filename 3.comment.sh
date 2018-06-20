9e41b0aa8873daa263ff66fca8753ac1cd1a9304
svzhk1 <39025839+svzhk1@users.noreply.github.com>
2018-06-20 09:30:16 +0530

    working with script comment

---
+++ 3.comment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash  
+#Way to add command 
+user="Saurav Vaid"        #creating new varibale
+datec=`date`              
+admin="svisback@gmail.com"
+#we are going to call those varibale in the script
+echo ""
+echo "=============================="
+echo "script owner: $user "
+echo "create date : $datec "
+echo "contact admin : $admin "
+echo "=============================="
+echo " "
+
+
+
+
